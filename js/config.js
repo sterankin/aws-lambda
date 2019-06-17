1fb432cfcba6f3f898dd222517388cfa6060f424
Steven Rankin <sterankin@hotmail.com>
2019-06-17 11:30:07 +0100

    updated config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_d8VtvSoIi', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6kp1ebs3kl4jstinc29kdc8e5t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
